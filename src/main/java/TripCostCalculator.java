e1e7a1a78ad1b63b7462d64cfc996d83ac75f6b5
Itz-Ezu <eetu.oinonen@metropolia.fi>
2025-03-05 15:03:36 +0200

    JOOOOO

--- src/main/java/TripCostCalculator.java
+++ src/main/java/TripCostCalculator.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TripCostCalculator {
+    public static double calculateTripCost(double distance, double fuelEfficiency, double fuelCost) {
+        return distance / fuelEfficiency * fuelCost;
+    }
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter the distance of the trip in km: ");
+        double distance = scanner.nextDouble();
+        double fuelEfficiency = 20; // 20 km per litre
+        System.out.println("Enter the cost of fuel per litre: ");
+        double fuelCost = scanner.nextDouble();
+        double tripCost = calculateTripCost(distance, fuelEfficiency, fuelCost);
+        System.out.println("The cost of the trip is: " + tripCost +"€");
+    }
+
 }
